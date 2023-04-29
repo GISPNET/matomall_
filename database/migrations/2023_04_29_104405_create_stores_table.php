b77552b76f3a9de5e853278b4b30a0aa223b4d8a
Matondo <matondojoaokitemoco@gmail.com>
2023-04-29 11:58:48 +0100

    Executando Migrações

--- database/migrations/2023_04_29_104405_create_stores_table.php
+++ database/migrations/2023_04_29_104405_create_stores_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
-            $table->string('');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('mobile_phone')->nullable();
+            $table->string('slug')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
