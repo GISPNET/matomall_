3e021abd7ada410f5a94deb23b6d1a7c5e934fbe
matondojoao <matondojoaokitemoco@gmail.com>
2023-05-19 11:09:54 +0100

    Resentando senha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,6 +10,22 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon.png') }}">
     <title>MatoMall - Login</title>
     <link href="{{asset('dist/css/style.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            // Oculta o formulário de login e exibe o formulário de recuperação de senha
+            $('#to-recover').on("click", function() {
+                $("#loginform").slideUp();
+                $("#recoverform").fadeIn();
+            });
+
+            // Oculta o formulário de recuperação de senha e exibe o formulário de login
+            $('#to-login').click(function() {
+                $("#recoverform").hide();
+                $("#loginform").fadeIn();
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -26,66 +42,72 @@
                     <div class="text-center p-t-20 p-b-20">
                         <span class="db"><img src="{{asset('assets/images/logo.png') }}" alt="logo" /></span>
                     </div>
-                    <form class="form-horizontal m-t-20" method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="row p-b-30">
-                            <div class="col-12">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text bg-success text-white" id="basic-addon1"><i class="ti-user"></i></span>
+                    <div id="loginform">
+                        <form class="form-horizontal m-t-20" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="row p-b-30">
+                                <div class="col-12">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-success text-white" id="basic-addon1"><i class="ti-user"></i></span>
+                                        </div>
+                                        <input type="text" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus placeholder="Email">
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                     </div>
-                                    <input type="text" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus placeholder="Email">
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                                </div>
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text bg-warning text-white" id="basic-addon2"><i class="ti-pencil"></i></span>
-                                    </div>
-                                    <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password"  autocomplete="current-password" placeholder="Senha">
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row border-top border-secondary">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <div class="p-t-20">
-                                        <button class="btn btn-info" id="to-recover" type="button">
-                                            <a href="{{ route('password.request') }}"><i class="fa fa-lock m-r-5"></i> Senha perdida ?</a></button>
-                                          <button class="btn btn-success float-right" type="submit">Entrar</button>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-warning text-white" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                        </div>
+                                        <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password"  autocomplete="current-password" placeholder="Senha">
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+                            <div class="row border-top border-secondary">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <div class="p-t-20">
+                                            <button class="btn btn-info" id="to-recover" type="button"><i class="fa fa-lock m-r-5"></i> Senha perdida?</button>
+                                              <button class="btn btn-success float-right" type="submit">Entrar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <div id="recoverform">
                     <div class="text-center">
-                        <span class="text-white">Enter your e-mail address below and we will send you instructions how to recover a password.</span>
+                        <span class="text-white">Digite seu endereço de e-mail abaixo e nós lhe enviaremos instruções de como recuperar sua senha.</span>
                     </div>
                     <div class="row m-t-20">
                         <!-- Form -->
-                        <form class="col-12" action="index.html">
-                            <!-- email -->
+                        <form class="col-12" action="{{ route('password.email') }}" method="POST" >
+                            @csrf
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text bg-danger text-white" id="basic-addon1"><i class="ti-email"></i></span>
                                 </div>
-                                <input type="text" class="form-control form-control-lg" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1">
+                               <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Endereço de email" autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                            <!-- pwd -->
                             <div class="row m-t-20 p-t-20 border-top border-secondary">
                                 <div class="col-12">
-                                    <a class="btn btn-success" href="#" id="to-login" name="action">Back To Login</a>
-                                    <button class="btn btn-info float-right" type="button" name="action">Recover</button>
+                                    <a class="btn btn-success" href="#" id="to-login" name="action">Volte ao login</a>
+                                    <button class="btn btn-info float-right" type="submit" name="action">Recuperar</button>
                                 </div>
                             </div>
                         </form>
@@ -93,32 +115,11 @@
                 </div>
             </div>
         </div>
-        <!-- ============================================================== -->
-        <!-- Login box.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper scss in scafholding.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper scss in scafholding.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right Sidebar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right Sidebar -->
-        <!-- ============================================================== -->
     </div>
-    <!-- ============================================================== -->
-    <!-- All Required js -->
-    <!-- ============================================================== -->
-    <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
-    <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- ============================================================== -->
-    <!-- This page plugin js -->
-    <!-- ============================================================== -->
+
+    <script src="{{asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <script>
 
     $('[data-toggle="tooltip"]').tooltip();
