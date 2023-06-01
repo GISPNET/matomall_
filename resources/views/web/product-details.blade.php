12df59f6d55eb59e8fae119f1001248ca6c54b1a
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-01 12:06:49 +0100

    Iniciando Adição de Produto no Carrinho

--- resources/views/web/product-details.blade.php
+++ resources/views/web/product-details.blade.php
@@ -87,6 +87,10 @@
                                     <span class="me-2 far fa-heart"></span>Adicionar a lista de desejos
                                 </button>
                                 <form action="" method="post">
+                                    <input type="hidden" name="product[name]" value="{{ $product->name }}">
+                                    <input type="hidden" name="product[price]"  value="{{ $product->price }}">
+                                    <input type="hidden" name="product[slug]"  value="{{ $product->slug }}">
+
                                     <input class="form-control" type="number" name="quantidade" id="quantidade" value="1">
                                     <button class="btn btn-lg btn-warning rounded-pill flex-fill fs--1 fs-sm-0">
                                         <span class="fas fa-shopping-cart me-2"></span> Adicionar ao carrinho
