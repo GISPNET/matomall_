4646710fcc0e16cd3d28dab2ddd7be37a05d4641
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-03 11:50:46 +0100

    Duplicidade de Itens no Carrinho

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function index(){
-        $carts=session()->has('cart')? session()->get('cart'): [];
-        return view('web.cart',compact(['carts']));
+    public function index()
+    {
+        $carts = session()->has('cart') ? session()->get('cart') : [];
+        return view('web.cart', compact(['carts']));
     }
 
     public function add(Request $request)
@@ -18,42 +19,45 @@ class CartController extends Controller
 
         if (session()->has('cart')) {
 
-            $productsSlungs=array_column($products,'slug');
-            if(in_array($product['slug'],$productsSlungs)){
-                   $this->productIncrement($product['slug'], $product['amount'], $products);
-                   $products=session()->get('cart');
+            $products = session()->get('cart');
+            $productsSlungs = array_column($products, 'slug');
+
+            if (in_array($product['slug'], $productsSlungs)) {
+                $products = $this->productIncrement($product['slug'], $product['amount'], $products);
+                session()->put('cart', $products);
+            }else{
+                session()->push('cart', $product);
             }
-            session()->push('cart', $product);
         } else {
             $products[] = $product;
             session()->put('cart', $products);
         }
-        return back()->with('product-added','Produto adicionado com sucesso');
+        return back()->with('product-added', 'Produto adicionado com sucesso');
     }
     public function remove($slug)
     {
         if (!session()->has('cart'))
-           return redirect()->route('cart.index');
+            return redirect()->route('cart.index');
 
-           $products=session()->get('cart');
+        $products = session()->get('cart');
 
-           $products= array_filter($products , function($line) use($slug){
-               return $line['slug'] != $slug;
-           });
+        $products = array_filter($products, function ($line) use ($slug) {
+            return $line['slug'] != $slug;
+        });
 
-           session()->put('cart', $products);
+        session()->put('cart', $products);
 
-           return redirect()->route('cart.index');
+        return redirect()->route('cart.index');
     }
-    private function productIncrement($slug,$amount, $products){
-        $products=array_map(function($line)use($slug,$amount){
-            if($slug==$line['slug']){
-                $line['amount']+=$amount;
+    private function productIncrement($slug, $amount, $products)
+    {
+        $products = array_map(function ($line) use ($slug, $amount) {
+            if ($slug == $line['slug']) {
+                $line['amount'] += $amount;
             }
             return $line;
-        },$products);
+        }, $products);
 
         return $products;
     }
-
 }
