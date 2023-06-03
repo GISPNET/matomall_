8bcfd74acec158489559eff586433e79dcc39d16
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-03 13:16:06 +0100

    Iniciando Checkout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,6 +43,7 @@ class LoginController extends Controller
     }
 
     protected function authenticated(Request $request, $user){
+
         if(session()->has('cart')){
             return redirect()->route('checkout.index');
         }
