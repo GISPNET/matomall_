fb1fbe8ab55d06e3701a9ea5418537e7a66c0bb1
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-09 21:22:23 +0100

    Mapeando relacionamento

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     {
         return $this->hasOne(Store::class);
     }
+    public function orders()
+    {
+        return $this->hasMany(UserOrder::class);
+    }
 }
