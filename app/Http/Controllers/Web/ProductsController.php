0a3fe439954dac0f410881e61f6fbff5eda28c93
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-21 18:59:43 +0100

    Armazenando em Cache

--- app/Http/Controllers/Web/ProductsController.php
+++ app/Http/Controllers/Web/ProductsController.php
@@ -5,19 +5,32 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        $products = Product::orderBy('id', 'DESC')->paginate(9);
-        return view('web.products',compact(['products']));
+        $expiracao = 1;
+
+        $products = Cache::remember('all_products', $expiracao, function () {
+            return Product::orderBy('id', 'DESC')->paginate(9);
+        });
+        return view('web.products', compact(['products']));
     }
 
     public function show($slug)
     {
-        $similar_products=Product::all();
-        $product = Product::where('slug', '=', $slug)->first();
-        return view('web.product-details',compact(['product','similar_products']));
+        $expiracao = 1;
+
+        $product = Cache::remember('product', $expiracao, function () use ($slug) {
+            return Product::where('slug', '=', $slug)->first();
+        });
+
+        $similar_products = Cache::remember('similar_products', $expiracao, function () {
+            return Product::all();
+        });
+
+        return view('web.product-details', compact(['product', 'similar_products']));
     }
 }
