f106858315894222516005ffc27d13d3a87c006f
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-23 14:41:34 +0100

    Iniciando a landing page do marketplace

--- resources/views/landing/default.blade.php
+++ resources/views/landing/default.blade.php
@@ -449,49 +449,39 @@
                     <div class="position-relative">
                         <div class="row mb-6">
                             <div class="col-xl-6 text-center text-md-start">
-                                <h2 class="text-white mb-2">Being used by millions of users</h2>
+                                <h2 class="text-white mb-2">Usado por milhões de usuários</h2>
                                 <h1 class="fs-md-5 fs-xl-6 fw-black text-gradient-info text-uppercase mb-4 mb-md-0">
-                                    WORLDWIDE</h1>
+                                    EM TODO O MUNDO</h1>
                             </div>
                             <div class="col-xl-6 text-center text-md-start">
-                                <p class="text-white">You can get all the reports, data analysis, and growth maps you
-                                    need with the help of Phoenix s power, and you may review and modify them whenever
-                                    you want. These features make this dashboard outstanding.</p>
+                                <p class="text-white">Você pode obter todos os relatórios, análises de dados e mapas de crescimento de que precisa com a ajuda do poder do Matomall, e você pode revisá-los e modificá-los sempre que quiser. Esses recursos tornam este painel excepcional.</p>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-xl-8 text-center text-md-start mb-6 mb-xl-0">
                                 <div class="d-md-flex justify-content-md-between">
                                     <div class="mb-6 mb-md-0 me-4">
-                                        <h1 class="display-1 text-white fw-bolder"
-                                            data-countup='{"endValue":125,"duration":10,"suffix":"+"}'>0 <span>+</span>
-                                        </h1>
-                                        <p class="text-white">Every month, there are more<br
-                                                class="d-md-none d-lg-block" />than 125+ sales.</p>
+                                        <h1 class="display-1 text-white fw-bolder" data-countup='{"endValue":125,"duration":10,"suffix":"+"}'>0 <span>+</span></h1>
+                                        <p class="text-white">A cada mês, há mais de<br class="d-md-none d-lg-block" />125+ vendas.</p>
                                     </div>
                                     <div class="mb-6 mb-md-0 me-4">
-                                        <h1 class="display-1 text-white fw-bolder"
-                                            data-countup='{"endValue":308,"duration":10,"suffix":"k"}'>0</h1>
-                                        <p class="text-white">We have 308+ active paid.<br
-                                                class="d-md-none d-lg-block" />subscribers.</p>
+                                        <h1 class="display-1 text-white fw-bolder" data-countup='{"endValue":308,"duration":10,"suffix":"k"}'>0</h1>
+                                        <p class="text-white">Temos mais de 308 assinantes<br class="d-md-none d-lg-block" />pagos ativos.</p>
                                     </div>
                                     <div class="mb-6 mb-md-0 me-4">
-                                        <h1 class="display-1 text-white fw-bolder"
-                                            data-countup='{"endValue":12,"duration":0.5}'>0</h1>
-                                        <p class="text-white">We have won 12 awards so<br
-                                                class="d-md-none d-lg-block" />far with great success. </p>
+                                        <h1 class="display-1 text-white fw-bolder" data-countup='{"endValue":12,"duration":0.5}'>0</h1>
+                                        <p class="text-white">Já ganhamos 12 prêmios<br class="d-md-none d-lg-block" />com grande sucesso.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-4 text-center text-md-start"><img class="img-fluid"
-                                    src="assets/img/generic/capterra.png" alt="" /></div>
+                            <div class="col-xl-4 text-center text-md-start"><img class="img-fluid" src="assets/img/generic/capterra.png" alt="" /></div>
                         </div>
                     </div>
                 </div>
-            </section><svg class="text-white w-100 position-relative" viewBox="0 0 1920 368" fill="none"
-                xmlns="http://www.w3.org/2000/svg">
+            </section>
+            <svg class="text-white w-100 position-relative" viewBox="0 0 1920 368" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 368L1920 0.730011L1920 368L0 368Z" fill="currentColor"></path>
-            </svg>
+            </svg
         </div>
 
         <section class="bg-white">
