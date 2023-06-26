dc70a6081376268eef5429af205dc25692795ede
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-26 12:49:28 +0100

    Iniciando a Seeder de Categorias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-           CategoriasSeederTable::class
+            CategoriesSeederTable::class
         ]);
     }
 }
