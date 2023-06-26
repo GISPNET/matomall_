032883ff4fe1f156ee0bb63c2f8112effa399b65
matondojoao <matondojoaokitemoco@gmail.com>
2023-06-26 12:59:02 +0100

    Iniciando a seeder de marcas

--- database/seeders/BrandsSeederTable.php
+++ database/seeders/BrandsSeederTable.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BrandsSeederTable extends Seeder
 {
@@ -14,6 +15,103 @@ class BrandsSeederTable extends Seeder
      */
     public function run()
     {
-        //
+        if (DB::table('brands')->count() < 0) {
+
+            DB::table('brands')->insert([
+                [
+                    'name' => 'Nike'
+                ],
+                [
+                    'name' => 'Adidas'
+                ],
+                [
+                    'name' => 'Apple'
+                ],
+                [
+                    'name' => 'Samsung'
+                ],
+                [
+                    'name' => 'Sony'
+                ],
+                [
+                    'name' => 'Microsoft'
+                ],
+                [
+                    'name' => 'HP'
+                ],
+                [
+                    'name' => 'Dell'
+                ],
+                [
+                    'name' => 'Lenovo'
+                ],
+                [
+                    'name' => 'Canon'
+                ],
+                [
+                    'name' => 'Nikon'
+                ],
+                [
+                    'name' => 'LG'
+                ],
+                [
+                    'name' => 'Bose'
+                ],
+                [
+                    'name' => 'Sony'
+                ],
+                [
+                    'name' => 'GoPro'
+                ],
+                [
+                    'name' => 'Coca-Cola'
+                ],
+                [
+                    'name' => 'Pepsi'
+                ],
+                [
+                    'name' => 'Nestlé'
+                ],
+                [
+                    'name' => 'Kellogg\'s'
+                ],
+                [
+                    'name' => 'Unilever'
+                ],
+                [
+                    'name' => 'Procter & Gamble'
+                ],
+                [
+                    'name' => 'L\'Oréal'
+                ],
+                [
+                    'name' => 'Gucci'
+                ],
+                [
+                    'name' => 'Prada'
+                ],
+                [
+                    'name' => 'Louis Vuitton'
+                ],
+                [
+                    'name' => 'Rolex'
+                ],
+                [
+                    'name' => 'Hermès'
+                ],
+                [
+                    'name' => 'Chanel'
+                ],
+                [
+                    'name' => 'Dior'
+                ],
+                [
+                    'name' => 'Zara'
+                ],
+                [
+                    'name' => 'H&M'
+                ],
+            ]);
+        }
     }
 }
