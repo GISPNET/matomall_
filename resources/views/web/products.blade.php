4ad40871b88881ce2e61259f26fbe0db3df00121
matondojoao <matondojoaokitemoco@gmail.com>
2023-07-22 13:09:18 +0100

    Exibindo correctamente a imagem do produto na tela de todos os produtos

--- resources/views/web/products.blade.php
+++ resources/views/web/products.blade.php
@@ -28,8 +28,14 @@
                                                             title="Add to wishlist"><span
                                                                 class="fas fa-heart d-block-hover"></span><span
                                                                 class="far fa-heart d-none-hover"></span></button>
+                                                                @if ($product->photos->count())
 
-                                                                <img class="img-fluid" src="{{ $product->photos->first()->image}}" alt="" />
+                                                                <img class="img-fluid" src="{{ Storage::url($product->photos->first()->image) }}" alt="" />
+
+                                                                @else
+                                                                  <img class="img-fluid" src="{{ asset('assets/images/matomall-placeholder.png') }}" alt="" />
+
+                                                                @endif
 
                                                     </div><a class="stretched-link text-decoration-none"
                                                         href="{{ route('product.details', $product->slug) }}">
