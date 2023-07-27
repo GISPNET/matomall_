882ee0076cded6aa9af7bbcd62b500a87cd7bfce
matondojoao <matondojoaokitemoco@gmail.com>
2023-07-27 13:14:08 +0100

    Melhorando a exibicao de menus

--- resources/views/sellers/components/navbar/navbar-vertical-navbar-expand-lg.blade.php
+++ resources/views/sellers/components/navbar/navbar-vertical-navbar-expand-lg.blade.php
@@ -134,15 +134,22 @@
                                                 </svg>
                                                 <!-- <span class="fas fa-caret-right"></span> Font Awesome fontawesome.com -->
                                             </div><span class="nav-link-icon">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text">
-                                                  <path d="M4 0h10a4 4 0 0 1 4 4v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"></path>
-                                                  <polyline points="14 2 14 8 20 8"></polyline>
-                                                  <line x1="16" y1="13" x2="8" y2="13"></line>
-                                                  <line x1="16" y1="17" x2="8" y2="17"></line>
-                                                  <polyline points="10 9 9 9 8 9"></polyline>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-file-text">
+                                                    <path
+                                                        d="M4 0h10a4 4 0 0 1 4 4v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z">
+                                                    </path>
+                                                    <polyline points="14 2 14 8 20 8"></polyline>
+                                                    <line x1="16" y1="13" x2="8"
+                                                        y2="13"></line>
+                                                    <line x1="16" y1="17" x2="8"
+                                                        y2="17"></line>
+                                                    <polyline points="10 9 9 9 8 9"></polyline>
                                                 </svg>
-                                              </span>
-                                              </span><span class="nav-link-text">Pedidos</span><svg
+                                            </span>
+                                            </span><span class="nav-link-text">Pedidos</span><svg
                                                 class="svg-inline--fa fa-circle text-info ms-1 new-page-indicator"
                                                 style="font-size: 6px;" aria-hidden="true" focusable="false"
                                                 data-prefix="fas" data-icon="circle" role="img"
@@ -248,18 +255,23 @@
                                                     </path>
                                                 </svg>
                                                 <!-- <span class="fas fa-caret-right"></span> Font Awesome fontawesome.com -->
-                                            </div><span class="nav-link-icon"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    width="16px" height="16px" viewBox="0 0 24 24" fill="none"
-                                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                    stroke-linejoin="round" class="feather feather-share-2">
-                                                    <circle cx="18" cy="5" r="3"></circle>
-                                                    <circle cx="6" cy="12" r="3"></circle>
-                                                    <circle cx="18" cy="19" r="3"></circle>
-                                                    <line x1="8.59" y1="13.51" x2="15.42"
-                                                        y2="17.49"></line>
-                                                    <line x1="15.41" y1="6.51" x2="8.59"
-                                                        y2="10.49"></line>
-                                                </svg></span><span class="nav-link-text">Promoções e Ofertas</span>
+                                            </div><span class="nav-link-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-gift">
+                                                    <polyline points="20 12 20 22 4 22 4 12"></polyline>
+                                                    <rect x="2" y="7" width="20"
+                                                        height="5"></rect>
+                                                    <line x1="12" y1="22" x2="12"
+                                                        y2="7"></line>
+                                                    <path d="M5 22V7"></path>
+                                                    <path d="M19 22V7"></path>
+                                                    <path d="M12 2V7"></path>
+                                                </svg>
+                                            </span>
+
+                                            <span class="nav-link-text">Promoções e Ofertas</span>
                                         </div>
                                     </a>
                                     <div class="parent-wrapper label-1">
@@ -360,12 +372,11 @@
                                             </div><span class="nav-link-icon"><svg xmlns="http://www.w3.org/2000/svg"
                                                     width="16px" height="16px" viewBox="0 0 24 24" fill="none"
                                                     stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                    stroke-linejoin="round" class="feather feather-clipboard">
+                                                    stroke-linejoin="round" class="feather feather-settings">
+                                                    <circle cx="12" cy="12" r="3"></circle>
                                                     <path
-                                                        d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2">
+                                                        d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
                                                     </path>
-                                                    <rect x="8" y="2" width="8"
-                                                        height="4" rx="1" ry="1"></rect>
                                                 </svg></span><span class="nav-link-text">Configurações
                                             </span>
                                         </div>
@@ -427,12 +438,21 @@
                                                         xmlns="http://www.w3.org/2000/svg" width="16px"
                                                         height="16px" viewBox="0 0 24 24" fill="none"
                                                         stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                        stroke-linejoin="round" class="feather feather-tag">
-                                                        <path
-                                                            d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z">
-                                                        </path>
-                                                        <line x1="7" y1="7" x2="7.01"
-                                                            y2="7"></line>
+                                                        stroke-linejoin="round" class="feather feather-life-buoy">
+                                                        <circle cx="12" cy="12" r="10">
+                                                        </circle>
+                                                        <circle cx="12" cy="12" r="4">
+                                                        </circle>
+                                                        <line x1="4.93" y1="4.93" x2="9.17"
+                                                            y2="9.17"></line>
+                                                        <line x1="14.83" y1="14.83" x2="19.07"
+                                                            y2="19.07"></line>
+                                                        <line x1="14.83" y1="9.17" x2="19.07"
+                                                            y2="4.93"></line>
+                                                        <line x1="14.83" y1="9.17" x2="18.36"
+                                                            y2="5.64"></line>
+                                                        <line x1="4.93" y1="19.07" x2="9.17"
+                                                            y2="14.83"></line>
                                                     </svg></span><span class="nav-link-text">Ajuda e Suporte:
                                                 </span>
                                             </div>
