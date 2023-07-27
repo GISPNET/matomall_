b6ae5cd087d98d3080c9f96c9eff45f5c1cff649
matondojoao <matondojoaokitemoco@gmail.com>
2023-07-27 15:15:57 +0100

    listando os clientes da loja

--- app/Http/Controllers/Seller/CustomerController.php
+++ app/Http/Controllers/Seller/CustomerController.php
@@ -16,32 +16,27 @@ class CustomerController extends Controller
 
     public function index()
     {
-        // Obter a loja do vendedor autenticado
         $store = Auth::user()->store;
 
-        // Obter os pedidos feitos na loja do vendedor autenticado
         $orders = $store->orders;
 
-        // Criar um array para armazenar os IDs de clientes únicos que fizeram pedidos na loja
         $uniqueCustomerIds = [];
 
         foreach ($orders as $order) {
-            // Verificar se o ID do cliente já existe no array de IDs únicos
+
             if (!in_array($order->user_id, $uniqueCustomerIds)) {
-                // Se o ID do cliente não existe, adicioná-lo ao array
+
                 $uniqueCustomerIds[] = $order->user_id;
             }
         }
 
-        // Obter os registros de clientes usando os IDs únicos
         $uniqueCustomers = User::whereIn('id', $uniqueCustomerIds)->paginate(10);
 
-        // Calcular o total gasto, número de pedidos, data do último pedido e data do último acesso para cada cliente na loja
         foreach ($uniqueCustomers as $customer) {
             $customer->totalSpent = 0;
             $customer->totalOrders = 0;
             $customer->lastOrderDate = null;
-            $customer->lastAccessDate = $customer->last_login_at; // Supondo que a coluna que armazena a data do último acesso do cliente seja 'last_login_at'
+            $customer->lastAccessDate = $customer->last_login_at;
 
             foreach ($orders as $order) {
                 if ($order->user_id === $customer->id) {
@@ -58,7 +53,6 @@ class CustomerController extends Controller
             }
         }
 
-        // Passar os dados para a view 'sellers.customers.all-customers'
         return view('sellers.customers.all-customers', compact('uniqueCustomers'));
     }
 }
